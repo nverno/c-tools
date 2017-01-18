0d7d132263395823dfcd532fb4cd5787fa07c3b9
nverno <noah.v.peart@gmail.com>
2017-01-18 13:24:24 -0500

    squash

--- c++-test.el
+++ c++-test.el
@@ -29,22 +29,47 @@
 (eval-when-compile
   (require 'nvp-macro)
   (require 'cl-lib))
+(require 'nvp-test)
+
+;; -------------------------------------------------------------------
+;;; Util
+
+(eval-when-compile
+ (defmacro with-c++-vars (&rest body)
+   (declare (indent defun))
+   `(nvp-with-project (:test-re ".*test.*\.cpp" :root "test")
+      ,@body)))
+
+;; compile and run boost.test in current buffer
+(defun c++-test-run-unit-test ()
+  (let* ((out (concat (nvp-bfn) (nvp-with-gnu/w32 ".out" ".exe")))
+         (compile-command
+         (concat (nvp-program "g++") " -std=c++11 -O3 -s -o " out
+                 " " (file-name-nondirectory buffer-file-name)
+                 " -lboost_unit_test_framework "
+                 "; ./" out "; rm " out))
+         (compilation-read-command nil))
+    (call-interactively 'compile)))
 
 ;; -------------------------------------------------------------------
 ;;; Commands 
 
-;; boost.test
-(defun c++-test-run-tests (arg)
-  (interactive "P")
-  (nvp-with-project
-    (:test-re ".*test.*\.cpp" :root "")
-    (message (nvp-test-select-test))))
+;; run boost.test
+;;;###autoload
+(defun c++-test-run-unit-tests ()
+  "Run unit tests in test directory, if more than one prompts for test file."
+  (interactive)
+  (with-c++-vars
+    (nvp-with-test nil (c++-test-run-unit-test))))
 
 ;;;###autoload
 (defun c++-test-jump-to-test ()
+  "Jump to tests in test directory."
   (interactive)
-  (nvp-with-project (:test-re ".*test.*\.cpp" :root "")
-    ))
+  (with-c++-vars
+    (nvp-with-test nil
+      (setq-local local-abbrev-table boost-abbrev-table)
+      (pop-to-buffer (current-buffer)))))
 
 (provide 'c++-test)
 ;;; c++-test.el ends here
